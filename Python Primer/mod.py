f0a947ca0dbe7ea1ca61218b11aee49540292207
Arew3y <jojilromma@gmail.com>
2025-11-08 16:32:01 +0530

    First chapter completed

---
+++ Python Primer/mod.py
@@ -0,0 +1,12 @@
+print("############# [ Module mod is loaded ] #############")
+
+def say_hello():
+    print("You just ran a module function")
+
+class suckk:
+    def __init__(self):
+        print("Hi, I'm a new class")
+        self.a = 1
+    def cls_love(self):
+        if self.a == 1:
+            print("You just ran a class method from a module")
